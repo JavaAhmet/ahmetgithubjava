e6fee22ea53f6e148b42870226bde90670b7276c
JavaAhmet <tefekahmet@gmail.com>
2023-08-14 02:54:02 +0300

    Gun03

--- src/Gun02/Student.java
+++ src/Gun02/Student.java
@@ -1,6 +1,12 @@
 package Gun02;
 
 public class Student {
-    String ad;
-    String soyad;
+    public static void main(String[] args) {
+        String ad;
+        String soyad;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("merhaba");
+        }
+    }
+    
 }
